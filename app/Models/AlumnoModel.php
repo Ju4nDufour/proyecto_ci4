ccb9ae29a2bda7e0fd2e9a69fb6ea15d7b9d79c5
LauraRodino <rodino.laura1@gmail.com>
2025-10-21 21:26:30 -0300

    fff

--- app/Models/AlumnoModel.php
+++ app/Models/AlumnoModel.php
@@ -1,28 +1,20 @@
-<?php namespace App\Models;
+<?php
+namespace App\Models;
 
 use CodeIgniter\Model;
 
 class AlumnoModel extends Model
 {
-    protected $table      = 'alumno';
+    protected $table = 'alumno';
     protected $primaryKey = 'id_alumno';
-    protected $allowedFields = ['dni','nombre','email','fecha_nac','id_carrera'];
+    protected $returnType = 'array';
+    protected $allowedFields = ['dni','nombre','apellido','email','fecha_nac','id_carrera'];
     protected $useTimestamps = false;
 
-
-
-    // Validación básica para crear
     protected $validationRules = [
-        'dni'        => 'required|exact_length[8]|is_natural_no_zero', // QUITAR is_unique
-        'nombre'     => 'required|min_length[3]',
-        'email'      => 'permit_empty|valid_email',
-        'fecha_nac'  => 'permit_empty|valid_date',
-        'id_carrera' => 'required|is_natural_no_zero',
-    ];
-    protected $validationMessages = [
-        'dni' => [
-            'is_unique'     => 'Ese DNI ya existe.',
-            'exact_length'  => 'El DNI debe tener 8 dígitos.'
-        ],
+        'dni' => 'required|exact_length[8]|numeric',
+        'nombre' => 'required|min_length[2]',
+        'apellido' => 'required|min_length[2]',
+        'email' => 'required|valid_email'
     ];
 }
--- app/Models/CursoModel.php
+++ app/Models/CursoModel.php
@@ -1,17 +1,19 @@
 <?php
 namespace App\Models;
+
 use CodeIgniter\Model;
 
 class CursoModel extends Model
 {
-    protected $table = 'Curso';
+    protected $table = 'curso';
     protected $primaryKey = 'id_curso';
-    protected $allowedFields = ['nombre', 'codigo', 'id_carrera', 'id_profesor'];
-
+    protected $returnType = 'array';
+    protected $allowedFields = ['nombre', 'id_carrera'];
+    protected $useTimestamps = false;
     protected $validationRules = [
-    'nombre' => 'required|min_length[3]',
-    'codigo' => 'required|alpha_numeric|max_length[10]',
-    'id_carrera' => 'required|is_natural_no_zero'
-];
-
+        'nombre' => 'required|min_length[3]',
+        'codigo' => 'required',
+        'id_carrera' => 'required|integer'
+    ];
 }
+
